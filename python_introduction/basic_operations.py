a325c68a6e0d13b9a1d183831648b46ad979b851
Rutomeshack18 <rutomeshack18@gmail.com>
2024-06-23 13:13:51 -0700

    Created README,md and basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Sum, difference and product operations on two numbers
+number1= 5
+number2= 10
+sum= number1 + number2
+difference= number2 - number1
+product= number1*number2
+print("Addition of", number2,"and", number1,"is", sum)
+print("Subtraction of", number2, "and", number1,"is", difference)
+print("Multiplication of", number2, "and", number1, "is", product)
