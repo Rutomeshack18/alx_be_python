2308e0839b55932b4ce5b64fcbf93416b2aff46a
Rutomeshack18 <rutomeshack18@gmail.com>
2024-07-20 19:49:57 -0700

    Modified robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -9,11 +9,10 @@ def safe_divide(numerator, denominator):
         return f"The result of the division is {result}"
     except ValueError:
         return "Error: Please enter numeric values only."
-    except ZeroDivisionError:  # Just in case, although float(denominator) == 0 should already catch this.
+    except ZeroDivisionError:
         return "Error: Cannot divide by zero."
 
 if __name__ == "__main__":
-    # This part will not execute when imported, only when run directly
     print(safe_divide(10, 5))
     print(safe_divide(10, 0))
     print(safe_divide('ten', 5))
